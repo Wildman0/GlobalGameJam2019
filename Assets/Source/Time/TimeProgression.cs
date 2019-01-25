86cd943e25a8610fa718cc4a71948bda4736a2f6
frantasticle <gitcommit621@gmail.com>
2019-01-25 22:29:20 +0000

    scripts for toggling lights, tracking time and updating clocks

---
+++ Assets/Source/Time/TimeProgression.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeProgression : MonoBehaviour {
+    private int Minutes;
+    private int UpdateMinutes = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        UpdateMinutes++;
+        if (UpdateMinutes == 50)
+        {
+            if (Minutes == 1440)
+            {
+                Minutes = 0;
+            }
+            else
+            {
+                Minutes += 2;
+            }
+        }
+    }
+    
+    public int GetMinutes ()
+    {
+        return Minutes;
+    }
+
+    public void SetMinutes (int NewMinutes)
+    {
+        Minutes = NewMinutes;
+    }
+}

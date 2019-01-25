1471dfdba816df2ddc9f65951acdcaad0eb7efbb
Wildman0 <kylewildman98@gmail.com>
2019-01-25 20:33:22 +0000

    Added button pressing scripts

---
+++ Assets/Source/ButtonPress.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{    
+	public void PressButton()
+    {
+        gameObject.SendMessage("OnButtonPress");
+    }
+}

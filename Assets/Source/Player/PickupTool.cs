7532e4a6cfc5a8359d69340596d268367c523ff8
Wildman0 <kylewildman98@gmail.com>
2019-01-25 21:28:51 +0000

    Put tool interaction in the right script

--- Assets/Source/Player/PickupTool.cs
+++ Assets/Source/Player/PickupTool.cs
@@ -6,14 +6,6 @@ public class PickupTool : MonoBehaviour
 {
 	void Update ()
     {
-        RaycastHit hit;
-
-        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 1.0f))
-        {
-            if (hit.transform.tag == "ToolInteractable")
-            {
-                hit.transform.gameObject.SendMessage("OnToolInteract");
-            }
-        }
+        
     }
 }

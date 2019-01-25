26d0b6fbf64d36cec9c98124171352acd026fbb7
Wildman0 <kylewildman98@gmail.com>
2019-01-25 21:33:15 +0000

    Added more tool functionality

--- Assets/Source/Player/PickupTool.cs
+++ Assets/Source/Player/PickupTool.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class PickupTool : MonoBehaviour
 {
-	void Update ()
+    ToolInHand toolInHand;
+
+    private void Start()
     {
-        
+        toolInHand = gameObject.GetComponent<ToolInHand>();
+    }
+
+    void Update ()
+    {
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 1.0f))
+        {
+            if (hit.transform.tag == "Tool")
+            {
+                toolInHand.toolType = hit.transform.gameObject.GetComponent<Tool>().toolType;
+            }
+        }
     }
 }

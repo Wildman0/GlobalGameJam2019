cc673cc3c9b30981a9278d29e531fcdea4c749ec
Wildman0 <kylewildman98@gmail.com>
2019-01-26 20:16:33 +0000

    Added record player

---
+++ Assets/Source/Records/Record.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Record : MonoBehaviour
+{
+    GameObject player;
+    bool isPlaying;
+    [SerializeField] AudioSource src;
+
+    private void Start()
+    {
+        src = GetComponent<AudioSource>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void PlayRecord()
+    {
+        src.Play();
+        isPlaying = true;
+    }
+
+    public void StopRecord()
+    {
+        src.Stop();
+        isPlaying = false;
+    }
+
+    void Update()
+    {
+        if (isPlaying)
+        {
+            src.volume = Mathf.Clamp(2 / (Vector3.Distance(player.transform.position, gameObject.transform.position)), 0, 1);
+
+            if (src.volume < 0.1f)
+                src.volume = 0.0f;
+        }
+    }
+}
---
+++ Assets/Source/Records/RecordPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject recordAttachPoint;
+
+    private void Start()
+    {
+        recordAttachPoint = transform.Find("DiscOrigin").gameObject;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Record")
+        {
+            AttachRecord(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Record")
+        {
+            collision.gameObject.GetComponent<Record>().StopRecord();
+        }
+    }
+
+    void AttachRecord(GameObject go)
+    {
+        go.transform.parent = recordAttachPoint.transform;
+        go.GetComponent<Record>().PlayRecord();
+        Debug.Log("ye");
+    }
+}

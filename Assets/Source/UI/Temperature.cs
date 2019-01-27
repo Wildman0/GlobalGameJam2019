d053ee0ba8a8ae10dc2854580d15ecf31a9484ee
Wildman0 <kylewildman98@gmail.com>
2019-01-27 13:23:35 +0000

    Added temperature UI

---
+++ Assets/Source/UI/Temperature.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Temperature : MonoBehaviour
+{
+    Image temperature;
+
+    private void Start()
+    {
+        temperature = gameObject.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        temperature.fillAmount = PlayerTemperature.getInstance.temperature / 20.0f;
+    }
+}
